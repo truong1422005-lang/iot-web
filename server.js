14443462d9d37b3036f8e7c295c9d77010ac34f8
truong <truong1422005@gmail.com>
2026-05-07 19:31:59 +0700

    fix iot server

--- server.js
+++ server.js
@@ -1,63 +1,44 @@
 ﻿const express = require("express");
-const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.json());
 
-// ===== CONNECT MONGO =====
-mongoose.connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log("MongoDB Connected");
-}).catch(err => {
-    console.log("Mongo Error:", err);
-});
+let data = [];
 
-// ===== SCHEMA =====
-const SensorSchema = new mongoose.Schema({
-    temperature: Number,
-    humidity: Number,
-    light: Number,
-    time: String
-});
-
-const Sensor = mongoose.model("Sensor", SensorSchema);
-
-// ===== ROUTES =====
+// test web
 app.get("/", (req, res) => {
-    res.send("IoT Server + MongoDB Running");
+    res.send("SERVER OK RUNNING");
 });
 
-// ESP32 gửi data
-app.post("/data", async (req, res) => {
-    const data = new Sensor({
+// nhận data từ ESP32
+app.post("/data", (req, res) => {
+    const d = {
         temperature: req.body.temperature,
         humidity: req.body.humidity,
         light: req.body.light,
-        time: new Date().toISOString()
-    });
+        time: new Date()
+    };
 
-    await data.save();
+    data.push(d);
+
+    console.log("Received:", d);
 
     res.json({ success: true });
 });
 
 // lấy tất cả data
-app.get("/api/data", async (req, res) => {
-    const data = await Sensor.find();
+app.get("/api/data", (req, res) => {
     res.json(data);
 });
 
-// lấy mới nhất
-app.get("/data/latest", async (req, res) => {
-    const data = await Sensor.findOne().sort({ _id: -1 });
-    res.json(data || {});
+// lấy data mới nhất
+app.get("/data/latest", (req, res) => {
+    res.json(data[data.length - 1] || {});
 });
 
-// ===== PORT RAILWAY =====
-const PORT = process.env.PORT;
+// PORT Railway
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log("Running on " + PORT);
